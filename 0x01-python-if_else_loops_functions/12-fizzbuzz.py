c74679d079986d3b9f5df0523d5992652ff328e9
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-03-08 12:33:00 +0100

    Solution file for task 12

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 101):
+        if (i % 15) == 0:
+            print("FizzBuzz", end=("\n" if i == 100 else " "))
+        elif (i % 5) == 0:
+            print("Buzz", end=("\n" if i == 100 else " "))
+        elif (i % 3) == 0:
+            print("Fizz", end=("\n" if i == 100 else " "))
