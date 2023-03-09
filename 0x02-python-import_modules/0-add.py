9a3b1fae143f6b13fe916422abbac31d8343a8eb
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-03-09 16:12:50 +0100

    Solution file for task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 from add_0 import add
-import sys
 
-if __name__ != "__main__":
-    sys.exit(0)
-
-a = 1
-b = 2
-print("{0:d} + {1:d} = {2:d}".format(a, b, add(a, b)))
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    print("{0:d} + {1:d} = {2:d}".format(a, b, add(a, b)))
