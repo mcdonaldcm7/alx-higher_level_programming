ca8a79722a2978e20e1fe26d2f2f30233adf615c
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-03-09 18:04:37 +0100

    Solution file for task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 from sys import argv
 
-argc = len(argv)
-if (argc - 1) == 0:
-    print("0 arguments.")
-elif (argc - 1) == 1:
-    print("1 argument:\n1: {0}".format(argv[1]))
-else:
-    print("{0:d} arguments:".format(argc - 1))
-    for i in range(1, argc):
-        print("{0:d}: {1:s}".format(i, argv[i]))
+if __name__ == "__main__":
+    argc = len(argv)
+    if (argc - 1) == 0:
+        print("0 arguments.")
+    elif (argc - 1) == 1:
+        print("1 argument:\n1: {0}".format(argv[1]))
+    else:
+        print("{0:d} arguments:".format(argc - 1))
+        for i in range(1, argc):
+            print("{0:d}: {1:s}".format(i, argv[i]))
