c9a987782a05def5041acc74e4a358d2b6acdf07
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-03-10 04:55:50 +0100

    Solution file for task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,8 +3,8 @@ from calculator_1 import add, sub, mul, div
 from sys import exit, argv
 
 if __name__ == "__main__":
-    a = int(argv[1])
-    b = int(argv[3])
+    a = int(argv[1].split(".")[0])
+    b = int(argv[3].split(".")[0])
 
     def result(opr):
         if opr == '+':
@@ -14,6 +14,8 @@ if __name__ == "__main__":
         elif opr == '*':
             return (mul(a, b))
         elif opr == '/':
+            if b == 0:
+                return (0)
             return (div(a, b))
         return (None)
 
@@ -24,5 +26,5 @@ if __name__ == "__main__":
         if argv[2] not in ['+', '-', '*', '/']:
             print("Unknown operator. Available operators: +, -, * and / ")
             exit(1)
-        print("{0:d} {2:s} {1:d} = {3:d}".format(a, b, argv[2],
-                                                 result(argv[2])))
+        print("{0:d} {2:s} {1:d} = {3:d}".format(
+            a, b, argv[2], result(argv[2])))
