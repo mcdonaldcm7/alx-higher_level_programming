9b3a11431b8659926dd51a0450e5d2d5cffba65f
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-03-10 04:06:50 +0100

    Solution files for task 6

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+from sys import exit, argv
+
+a = int(argv[1])
+b = int(argv[3])
+
+def result(opr):
+    if opr == '+':
+        return (add(a, b))
+    elif opr == '-':
+        return (sub(a, b))
+    elif opr == '*':
+        return (mul(a, b))
+    elif opr == '/':
+        return (div(a, b))
+    return (None)
+
+if __name__ == "__main__":
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    if argv[2] not in ['+', '-', '*', '/']:
+        print("Unknown operator. Available operators: +, -, * and / ")
+        exit(1)
+    print("{0:d} {2:s} {1:d} = {3:d}".format(a, b, argv[2], result(argv[2])))
