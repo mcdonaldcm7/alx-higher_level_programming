fab71e1cf56d6056027e78abe45e9474d600d792
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-03-10 07:01:16 +0100

    Solution file for task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,6 +3,13 @@ from calculator_1 import add, sub, mul, div
 from sys import exit, argv
 
 if __name__ == "__main__":
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    if argv[2] not in ['+', '-', '*', '/']:
+        print("Unknown operator. Only: +, -, * and / available")
+        exit(1)
+
     a = int(argv[1])
     b = int(argv[3])
 
@@ -19,12 +26,5 @@ if __name__ == "__main__":
             return (div(a, b))
         return (None)
 
-    if __name__ == "__main__":
-        if len(argv) != 4:
-            print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-            exit(1)
-        if argv[2] not in ['+', '-', '*', '/']:
-            print("Unknown operator. Only: +, -, * and / available")
-            exit(1)
-        print("{0:d} {2:s} {1:d} = {3:d}".format(
+    print("{0:d} {2:s} {1:d} = {3:d}".format(
             a, b, argv[2], result(argv[2])))
