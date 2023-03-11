294bfe88ccd82259bb61ccc3ee7e6fc920ddc94a
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-03-11 22:32:26 +0100

    Solution file for task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
-    if (idx < 0) or idx > (len(my_list) - 1):
+    size = len(my_list)
+    if (idx < 0) or (idx > (size - 1) and size != 0):
         return (None)
     copy = my_list.copy()
-    copy[idx] = element
+    if size == 0:
+        copy.append(element)
+    else:
+        copy[idx] = element
     return (copy)
