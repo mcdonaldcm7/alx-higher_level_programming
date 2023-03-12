5e037dda85ed1bc164645e3881289655e3bf2f25
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-03-12 17:44:53 +0100

    Solution file for task 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list is None or len(my_list) == 0:
+        return (None)
+    mx = 0
+    for num in my_list:
+        if num > mx:
+            mx = num
+    return (mx)
