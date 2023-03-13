e10bcfeb96ae6f1cffdbf7dfaf9aa3b8d3ec2e97
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-03-13 18:13:14 +0100

    Solution file for task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * get_2half - Returns a pointer to the halfpoint of the singly linked list
+ *
+ * @head: Pointer to the head of the singly linked list
+ *
+ * Return: Pointer to the midpoint of the list
+ */
 listint_t *get_2half(listint_t *head)
 {
 	listint_t *fast, *slow;
@@ -16,6 +23,14 @@ listint_t *get_2half(listint_t *head)
 	return (slow);
 }
 
+/**
+ * push_node - Uses a stack technique to reverse the midpoint of the list
+ *
+ * @head: Pointer to the head of the list
+ * @prev: Pointer to the previous node of the list
+ *
+ * Return: Pointer to the new head of the list
+ */
 listint_t *push_node(listint_t *head, listint_t *prev)
 {
 	listint_t *new;
@@ -28,6 +43,13 @@ listint_t *push_node(listint_t *head, listint_t *prev)
 	return (new);
 }
 
+/**
+ * is_palindrome - Checks if a singly linked list is a palindrome
+ *
+ * @head: Pointer to the head of the singly linked list
+ *
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
 int is_palindrome(listint_t **head)
 {
 	listint_t *half, *revhead, *slow, *fast, *prev, *iter;
@@ -48,7 +70,7 @@ int is_palindrome(listint_t **head)
 	}
 
 	iter = revhead;
-	while(fast != (void *) 0 && fast->next != (void *) 0)
+	while (fast != (void *) 0 && fast->next != (void *) 0)
 	{
 		if (slow->n != iter->n)
 		{
