697ee193d78e50a7a65d4ad907b757768a7cd86c
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-03-27 13:13:28 +0100

    A Safe list printing

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    r = 0
+    for i in range(x):
+        try:
+            print("{0}".format(my_list[i]), end="")
+            r += 1
+        except IndexError:
+            print()
+            return (i)
+    print()
+    return (r)
