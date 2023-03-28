4cdf20b1a3b9ad4dfc3a4d9b1f0c69d642d9e7dc
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-03-28 14:56:51 +0100

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module contains a simple Square class for task 1 (Square with size)
+"""
+
+class Square:
+    """A simple class with one public instance variable
+
+    The __init__ method initializes the size attribute
+
+    Attributes:
+        __size: Size attribute for the square
+    """
+    def __init__(self, size):
+        self.__size = size
