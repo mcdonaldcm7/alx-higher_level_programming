06f62cc1d18c89e7954bce2998e346253a6375d3
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-03-28 14:25:14 +0100

    Resolved pycodestyle error

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""
+This module contains a very simple empty class Square
+As was requested for task 0 of the Classes and Objects project
+"""
+
+
 class Square:
+
     """Square is an empty class created for the task 0
 
     The task name is My first square
@@ -7,5 +14,6 @@ class Square:
     The __init__ method is the method used to initialize instance variables
     or to override class variables
     """
+
     def __init__(self):
         pass
