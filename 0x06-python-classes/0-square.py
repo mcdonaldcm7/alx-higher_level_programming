e6e038cc013377112c6d0c7d4e3f905697b2fb01
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-03-28 14:17:39 +0100

    A module containing an empty square class

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+class Square:
+    """Square is an empty class created for the task 0
+
+    The task name is My first square
+
+    The __init__ method is the method used to initialize instance variables
+    or to override class variables
+    """
+    def __init__(self):
+        pass
