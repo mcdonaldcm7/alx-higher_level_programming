75bf503bbb12e2b7c11f0106cde5c4deea7aa2c2
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-03-29 10:33:59 +0100

    Added type checks for individual values of position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -29,10 +29,13 @@ class Square:
                 raise ValueError("size must be >= 0")
         else:
             raise TypeError("size must be an integer")
-        if (type(position) is not tuple or len(position) != 2
-                or position[0] < 0 or position[1] < 0):
+        if type(position) is not tuple or len(position) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if (type(position[0]) is not int or type(position[1]) is not int):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = position
+        if (self.__position[0] < 0 or self.__position[1] < 0):
+            raise TypeError("position must be a tuple of 2 positive integers")
 
     def area(self):
         """
