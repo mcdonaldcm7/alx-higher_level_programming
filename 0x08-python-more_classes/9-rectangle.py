e30dc7b6c9562a3e085e43e0829553773d35cbf5
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-04-03 17:52:46 +0100

    Added method to return a square instance of the Rectangle class

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -166,4 +166,8 @@ class Rectangle:
         This class method returns a new instance of the Rectangle class with
         width == height == size (In other words a square :)
         """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         return (Rectangle(size, size))
