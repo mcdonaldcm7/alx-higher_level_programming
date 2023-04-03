5cf9fadbea3a753b7e22a6ce9da8ef027a19fe5e
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-04-03 12:44:31 +0100

    If width or height is 0, perimeter is 0wq

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -26,14 +26,14 @@ class Rectangle:
     """
 
     def __init__(self, width=0, height=0):
-        if not isinstance(width, int):
-            raise TypeError("width must be an integer")
-        if width < 0:
-            raise ValueError("width must be >= 0")
         if not isinstance(height, int):
             raise TypeError("height must be an integer")
         if height < 0:
-            raise TypeError("height must be >= 0")
+            raise ValueError("height must be >= 0")
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise TypeError("width must be >= 0")
         self.__height = height
         self.__width = width
 
@@ -99,4 +99,6 @@ class Rectangle:
         Returns:
             Perimeter of the Rectangle i.e 2 * (width + height)
         """
+        if self.__width == 0 or self.__height == 0:
+            return (0)
         return (2 * (self.__width + self.__height))
