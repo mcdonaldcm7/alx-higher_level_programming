6dc68e36007121a484bc1916c851ddb87b8dced7
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-04-03 12:40:38 +0100

    Re-ordered the safety checks in the init method

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -18,19 +18,19 @@ class Rectangle:
     i.e Only integers >= 0
 
     Attributes:
-        width (int): Width of the rectangle
         height (int): Height of the rectangle
+        width (int): Width of the rectangle
     """
 
     def __init__(self, width=0, height=0):
-        if not isinstance(width, int):
-            raise TypeError("width must be an integer")
-        if width < 0:
-            raise ValueError("width must be >= 0")
         if not isinstance(height, int):
             raise TypeError("height must be an integer")
         if height < 0:
-            raise TypeError("height must be >= 0")
+            raise ValueError("height must be >= 0")
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise TypeError("width must be >= 0")
         self.__height = height
         self.__width = width
 
