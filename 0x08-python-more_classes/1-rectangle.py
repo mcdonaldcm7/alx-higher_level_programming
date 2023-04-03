dbba19e220c0b6398c3bf7f3f018d4c7a8fd47cb
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-04-03 18:15:03 +0100

    Corrected the safety check

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -30,7 +30,7 @@ class Rectangle:
         if not isinstance(width, int):
             raise TypeError("width must be an integer")
         if width < 0:
-            raise TypeError("width must be >= 0")
+            raise ValueError("width must be >= 0")
         self.__height = height
         self.__width = width
 
