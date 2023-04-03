c39da7262a028753a2612b755e580c2a80712a80
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-04-03 12:07:11 +0100

    Formatted the __init__

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -31,8 +31,8 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if height < 0:
             raise TypeError("height must be >= 0")
-        self.__width = width
         self.__height = height
+        self.__width = width
 
     @property
     def width(self):
