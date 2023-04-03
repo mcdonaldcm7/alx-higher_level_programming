2ff4c131f290c267dac2ff1c35b531ed671a9657
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-04-03 10:56:33 +0100

    Solution file for task 'Simple rectangle'

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module contains the definition for a simple and empty Rectangle class
+"""
+
+
+class Rectangle:
+    """
+    A simple empty class name Rectangle
+    """
