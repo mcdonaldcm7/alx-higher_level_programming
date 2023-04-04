55aa07bed9b72d04ea9ee29641b0614816749872
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-04-04 16:52:55 +0100

    Solution file for task 30 'Low memory cost'

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+"""
+This module defines a simple class that only allows the user to create a new
+instance attribute, if it is called 'first_name'
+"""
+
+
 class LockedClass:
     __slots__ = ("first_name",)
