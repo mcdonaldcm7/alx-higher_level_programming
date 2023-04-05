be1e08f9226d4383b0d8c948ef15cae550d624a2
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-04-05 12:23:55 +0100

    Module containing definition for add_integer

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -20,12 +20,8 @@ def add_integer(a, b=98):
 
     Return (a + b)
     """
-    result = 0
     if type(a) is not float and type(a) is not int:
         raise TypeError("a must be an integer")
     if type(b) is not float and type(b) is not int:
         raise TypeError("b must be an integer")
-    result = a + b
-    if result == float('inf') or result == -float('inf'):
-        return (89)
     return (int(a) + int(b))
