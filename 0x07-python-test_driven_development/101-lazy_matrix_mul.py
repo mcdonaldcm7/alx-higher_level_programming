e5e8df82e325ef5de59ce4c8535478b1437d3d60
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-04-07 18:48:01 +0100

    Module containing the function definition for the lazy_matrix_mul function

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This module contains the definition for the lazy_matrix_mul method which
+returns the product of the two matrices passed. It utilizes the numpy module
+for the computation hence the lazy added to the function name
+"""
+
+import numpy as np
+
+
+def matrix_mul(m_a, m_b):
+    """
+    This function multiplies and returns the product of two matrices created
+    using the numpy module.
+
+    Paramete(s)
+    -----------
+    m_a (list): First matrix
+    m_b (list): Second matrix
+
+    Returns:
+        m_a * m_b
+    """
+    return (np.matmul(m_a, m_b))
