59ba53513f506d5f31f9625821b0df73c89989f6
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-04-13 17:43:09 +0100

    Modified the BaseGeometry class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module contains an empty class called BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    Empty Class with an unimplemented function
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
