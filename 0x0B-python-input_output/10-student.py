75d5450a688ba5b6aa66dc4814e31e6e6c0defcb
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-04-15 12:39:15 +0100

    Added more functionalities to the 'to_son' method in the Student class

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+This module contains the definition for the Student class.
+The student class defines its constructor and a 'to_json' function that
+retrieves a dictionary represetation of a Student instance
+"""
+
+
+class Student:
+    """
+    A simple class representing a 'Student' with puclic instance attributes:
+        - first_name
+        - last_name
+        - age
+
+    This class also contains a method by name 'to_json' which returns a
+    dictionary representation of a Student instance
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance
+
+        attrs is the list of attributes to return, if attrs is None return all
+        """
+
+        if attrs is None:
+            return ({
+                        'age': self.age,
+                        'last_name': self.last_name,
+                        'first_name': self.first_name
+                })
+        else:
+            dic = {}
+            for i in range(-1, (len(attrs))):
+                if attrs[i] == 'first_name':
+                    dic[attrs[i]] = self.first_name
+                elif attrs[i] == 'last_name':
+                    dic[attrs[i]] = self.last_name
+                elif attrs[i] == 'age':
+                    dic[attrs[i]] = self.age
+            return (dic)
