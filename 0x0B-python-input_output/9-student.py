4bcb2f1b5f0044020c42f48190227e83b48bcc01
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-04-15 11:20:49 +0100

    A module containing a class Student that defines a Student by 3 attributes and also includes a method to retrieve a dictionary representation of a Student instance

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+This module contains the definition for the Student class.
+The student class defines its constructor and a 'to_json' function that
+retrieves a dictionary represetation of a Student instance
+"""
+
+
+class Student:
+    """
+    A simple class representing a 'Student' with puclic instance attributes:
+        - first_name
+        - last_name
+        - age
+
+    This class also contains a method by name 'to_json' which returns a
+    dictionary representation of a Student instance
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return ({
+                    'first_name' : self.first_name,
+                    'last_name' : self.last_name,
+                    'age' : self.age
+            })
