86deb42d1d9e1b41d436b5379fba1d07e28f8f97
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-04-15 11:23:24 +0100

    Reformatted code to pycodestyle

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -24,7 +24,7 @@ class Student:
 
     def to_json(self):
         return ({
-                    'first_name' : self.first_name,
-                    'last_name' : self.last_name,
-                    'age' : self.age
+                    'first_name': self.first_name,
+                    'last_name': self.last_name,
+                    'age': self.age
             })
