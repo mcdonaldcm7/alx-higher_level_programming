b06c7db3733fe52ef7eeee59c4fe5b06e26d6af3
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-04-18 07:06:22 +0100

    Documented module

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,20 +1,44 @@
 #!/usr/bin/python3
+"""
+This module contains the Square class which is a part of the 'models' package.
+It inherits the the Rectangle class and overrides some of it's inherited method
+"""
+
+
 from models.rectangle import Rectangle
 
+
 class Square(Rectangle):
+    """
+    A Square class which serves as representation of a real world Square object
+    with attributes and method to access, manipulate and define it's properties
+    """
+
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x=x, y=y, id=id)
 
     def __str__(self):
+        """
+        Defining the string representation of the Square class
+        """
+
         return ("[Square] ({0:d}) {1:d}/{2:d} - {3:d}\
                 ".format(self.id, self.x, self.y, self.width))
 
     @property
     def size(self):
+        """
+        Getter for the size property
+        """
+
         return (self.width)
 
     @size.setter
     def size(self, value):
+        """
+        Setter for the size property with type checks
+        """
+
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
@@ -23,6 +47,10 @@ class Square(Rectangle):
         self.height 
 
     def update(self, *args, **kwargs):
+        """
+        Updates the attributes of the current instance
+        """
+
         if args is not None and len(args) > 0:
             index = 0
             for arg in args:
@@ -49,10 +77,13 @@ class Square(Rectangle):
                     self.y = value
 
     def to_dictionary(self):
+        """
+        Returns a dictionary representation of the current object instance
+        """
+
         return ({
             "id": self.id,
             "x": self.x,
             "size": self.size,
             "y": self.y
             })
-
