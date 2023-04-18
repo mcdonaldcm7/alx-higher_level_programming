f0b16f08e705211a61094890543e37a5495cc500
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-04-18 05:35:07 +0100

    Module containing the Base class which other clsses will inherit

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""
+This module contains the 'Base' class which will serve as the base of other
+classes
+"""
+
+
+import json
+
+
+class Base:
+    """
+    This class will manage the id attribute in all future classes and to avoid
+    duplicating the same code
+    """
+
+    __nb_objects = 0
+    
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Returns the JSON string representation of list_dictionaries
+        """
+
+        return (json.dumps(list_dictionaries))
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        filename = "{0:s}.json".format(cls.__name__)
+
+        with open(filename, 'w') as f:
+            if list_objs is None or len(list_objs) == 0:
+                f.write(json.dumps([]))
+            else:
+                dict_list = []
+                for objs in list_objs:
+                    dic = objs.to_dictionary()
+                    dict_list.append(dic)
+                json_string = Base.to_json_string(dict_list)
+                f.write(json_string)
+
+    @staticmethod
+    def from_json_string(json_string):
+        return (json.loads(json_string))
+
+    @classmethod
+    def create(cls, **dictionary):
+        dummy = cls(3, 5, 1)
+        dummy.update(**dictionary)
+        return (dummy)
+
+    @classmethod
+    def load_from_file(cls):
+        pass
