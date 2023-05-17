baab02915d7651c0342d502503d6e902a3a4a678
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-05-17 22:28:23 +0100

    A script that lists all privileges of the MySQL users 'user_0d_1' and 'user_0d_2' on your server

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 -- A script that lists all privileges of the MySQL users 'user_0d_1' and
 -- 'user_0d_2' on your server (in localhost)
-SHOW GRANT FOR 'user_0d_1', 'user_0d_2';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
