1a5f5dff29fd267b3e210b0876b9dc6dca9f2bcc
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-05-18 21:54:21 +0100

    Output name is 'title' not 'name'

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,6 +1,6 @@
 -- A script that uses 'hbtn_0d_tvshows' database to list all genres of the show
 -- 'Dexter'
-SELECT tv_shows.title AS name
+SELECT tv_shows.title
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
