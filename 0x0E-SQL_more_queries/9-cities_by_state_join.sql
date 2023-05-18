43b2bba42afbd38c6590394d9df78dd0fb28a67f
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-05-18 05:41:55 +0100

    Added the ON condition

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,5 @@
 -- A script that lists all cities contained in the database 'hbtn_0d_usa'
-SELECT cities.id, cities.name, state.name
-FROM cities INNER JOIN state
+SELECT cities.id, cities.name, states.name
+FROM cities INNER JOIN states
+ON cities.id = states.id
 ORDER BY cities.id ASC;
