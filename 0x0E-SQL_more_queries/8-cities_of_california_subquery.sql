048999b28fdf6d8a3aaf0cddbc882c4141b9174a
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-05-18 05:32:33 +0100

    A script that lists all the cities of California that can be found in the database 'hbtn_0d_usa'

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,6 @@
 -- A script that lists all the cities of California that can be found in the
 -- database hbtn_0d_usa
-SELECT id, name
-FROM states
+SELECT states.id, name
+FROM states, cities
 WHERE name='California'
-ORDER BY id ASC;
+ORDER BY cities.id ASC;
