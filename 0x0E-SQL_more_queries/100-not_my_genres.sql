fc7e3da169717b91ff4a44ba4aef914a7762171b
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-05-18 23:56:31 +0100

    Output ordered by tv_genres.name

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -7,4 +7,5 @@ WHERE tv_genres.name NOT IN (
 	FROM tv_genres
 	LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 	LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-	WHERE tv_shows.title = 'Dexter');
+	WHERE tv_shows.title = 'Dexter')
+ORDER BY tv_genres.name ASC;
