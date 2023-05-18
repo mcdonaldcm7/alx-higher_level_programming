6c43cb0c1b0211634565cedf04aca7611f2c89ad
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-05-18 05:24:00 +0100

    Used backticks instead of single quotes

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,7 +2,7 @@
 -- database 'hbtn_0d_usa) on your MySQL server
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa
-CREATE TABLE IF NOT EXISTS 'cities'(
+CREATE TABLE IF NOT EXISTS `cities`(
 	id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(256) NOT NULL),
 	state_id INTEGER, FOREIGN KEY(state_id) REFERENCES state(id);
