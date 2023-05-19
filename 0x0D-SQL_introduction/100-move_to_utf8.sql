8abf97f4aa4714bd51f20c2e29bd14e041f9d020
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-05-19 21:22:58 +0100

    No CHARACTER set

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -4,8 +4,7 @@ ALTER DATABASE hbtn_0c_0
 CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE hbtn_0c_0.first_table
-CONVERT TO CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
+CONVERT TO COLLATE utf8mb4_unicode_ci;
 ALTER TABLE hbtn_0c_0.first_table
 MODIFY COLUMN name
 VARCHAR(256) COLLATE utf8mb4_unicode_ci;
