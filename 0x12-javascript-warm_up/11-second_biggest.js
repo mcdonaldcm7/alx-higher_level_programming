faebe34e9a8366c6d91a14c8e6d877f5657583de
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-06-13 17:11:41 +0100

    A script that searches the second biggest integer in the list of argument

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,14 +2,14 @@
 const len = process.argv.length - 2;
 if (len <= 0) console.log(0);
 else {
-  let max = 0;
-  let secMax = 0;
+  let max = process.argv[len];
+  let secMax = process.argv[len];
   for (let i = 0; i < len; i++) {
     const num = Number(process.argv[2 + i]);
-    if (num > max) {
+    if (num >= max) {
       secMax = max;
       max = num;
-    }
+    } else if (num > secMax) secMax = num;
   }
   console.log(secMax);
 }
