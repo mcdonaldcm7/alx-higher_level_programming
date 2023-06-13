d50794429543e327fb324d2a44f29a20a48765e3
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-06-13 13:04:31 +0100

    Resolved Semi-Standard error

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 function factorial (num) {
-	const x = Number(num);
-	if (isNaN(x) || x === 1 || x === 0) return (1);
-	return (x * factorial(x - 1));
+  const x = Number(num);
+  if (isNaN(x) || x === 1 || x === 0) return (1);
+  return (x * factorial(x - 1));
 }
 
 console.log(factorial(process.argv[2]));
