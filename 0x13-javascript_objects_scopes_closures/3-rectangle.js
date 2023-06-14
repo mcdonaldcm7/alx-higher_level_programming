4d1b52419947bc80bd3bd383e18814779001e822
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-06-14 13:12:24 +0100

    Added a print method to Rectangle class

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let rect = '';
+    for (let y = 0; y < this.height; y++) {
+      for (let x = 0; x < this.width; x++) {
+        rect += 'X';
+      }
+      if (y + 1 < this.height) rect += '\n';
+    }
+    console.log(rect);
+  }
+}
+
+module.exports = Rectangle;
