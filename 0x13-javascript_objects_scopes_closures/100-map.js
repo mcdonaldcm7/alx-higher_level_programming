3ed9c559653cb6a1f3b00cb2175f6944752d76d9
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-06-14 16:08:57 +0100

    Assigned computed array to a new list

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
 console.log(list);
-console.log(list.map(x => x * list.indexOf(x)));
+const newList = list.map(x => x * list.indexOf(x));
+console.log(newList);
