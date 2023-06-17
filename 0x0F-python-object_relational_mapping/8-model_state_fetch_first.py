796536b6c24800760651fd6fd547127c0669fe61
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-06-17 15:09:29 +0100

    A script that prints the first object in the table from the specified database

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This module makes use of the SQLAlchemy library to access and query the
+database whoose name is passed as a parameter to the command line.
+It import and uses the table defined by State to make it query, which retrieves
+and prints the first State object from the database
+"""
+
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    usr = sys.argv[1]
+    psswd = sys.argv[2]
+    db = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        usr, psswd, db), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for instance in session.query(State).order_by(State.id)[0:1]:
+        print("{}: {}".format(instance.id, instance.name))
