aedfa9051bc5437c713b9c804f6a83dd7d62f4f5
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-06-17 15:20:21 +0100

    Handled case: No record

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -24,4 +24,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     instance = session.query(State).order_by(State.id).first()
-    print("{}: {}".format(instance.id, instance.name))
+    if instance is None:
+        print("Nothing")
+    else:
+        print("{}: {}".format(instance.id, instance.name))
