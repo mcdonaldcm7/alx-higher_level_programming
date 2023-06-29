ecf653a564a759c5001750a7d092e39000ba109b
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-06-29 16:35:20 +0100

    Only print body of a 200 status code response

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 # Send a GET request to the URL, and display the body of the response
-curl -L -X "GET" --url $1
+#!/bin/bash
+# Send a GET request to the URL, and display the body of the response
+code=`curl -sI -w "%{http_code}" -X "GET" --url $1 | tail -n 1`
+if [ $code=="302" ]
+then
+        curl -L -X "GET" --url $1
+fi
