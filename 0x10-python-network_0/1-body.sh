355f0ec85f3b15fb17a2fee7253fc901a7902ed1
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-06-29 16:45:07 +0100

    Added bash header twice

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 # Send a GET request to the URL, and display the body of the response
-#!/bin/bash
-# Send a GET request to the URL, and display the body of the response
 code=$(curl -sI -w "%{http_code}" -X "GET" --url "$1" | tail -n 1)
 if [ "$code"=="302" ]
 then
