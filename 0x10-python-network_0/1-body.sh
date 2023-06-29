e5dd1434a57c07a4c258149cefbc5e6d54fe3bcf
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-06-29 17:05:51 +0100

    Compressed script to 3 lines

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 # Send a GET request to the URL, and display the body of the response
-host=$(echo "$1" | awk -F'/' '{print $1}')
-code=$(curl -sI -w "%{http_code}" -X "GET" --url "$host" | tail -n 1)
-if [ "$code"=="200" ]
-then
-        curl -L -X "GET" --url "$1"
-fi
+[ "$(curl -sI -w "%{http_code}" -X "GET" --url "$(echo "$1" | awk -F'/' '{print $1}')" | tail -n 1)" == "200" ] && curl -L -X "GET" --url "$1"
