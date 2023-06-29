ca31885732535d144b44bc20f4232d7cdc093ba2
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-06-29 16:59:22 +0100

    Checks initial page's status code not the redirect

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Send a GET request to the URL, and display the body of the response
-code=$(curl -sI -w "%{http_code}" -X "GET" --url "$1" | tail -n 1)
-if [ "$code"=="302" ]
+host=$(echo "$1" | awk -F'/' '{print $1}')
+code=$(curl -sI -w "%{http_code}" -X "GET" --url "$host" | tail -n 1)
+if [ "$code"=="200" ]
 then
         curl -L -X "GET" --url "$1"
 fi
