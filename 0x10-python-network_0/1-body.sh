c92027b4e866b78072d531c77399191aaf64ec0d
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-06-29 17:10:00 +0100

    Added option -s

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a GET request to the URL, and display the body of the response
-[ "$(curl -sI -w "%{http_code}" -X "GET" --url "$(echo "$1" | awk -F'/' '{print $1}')" | tail -n 1)" == "200" ] && curl -L -X "GET" --url "$1"
+[ "$(curl -sI -w "%{http_code}" -X "GET" --url "$(echo "$1" | awk -F'/' '{print $1}')" | tail -n 1)" == "200" ] && curl -s -L -X "GET" --url "$1"
