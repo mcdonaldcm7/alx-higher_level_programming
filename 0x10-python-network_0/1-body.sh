412ecfad8023d3b60a7e1d687635e7b934b5d78f
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-06-29 16:42:58 +0100

    Resolved shellcheck error

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -2,8 +2,8 @@
 # Send a GET request to the URL, and display the body of the response
 #!/bin/bash
 # Send a GET request to the URL, and display the body of the response
-code=`curl -sI -w "%{http_code}" -X "GET" --url $1 | tail -n 1`
-if [ $code=="302" ]
+code=$(curl -sI -w "%{http_code}" -X "GET" --url "$1" | tail -n 1)
+if [ "$code"=="302" ]
 then
-        curl -L -X "GET" --url $1
+        curl -L -X "GET" --url "$1"
 fi
