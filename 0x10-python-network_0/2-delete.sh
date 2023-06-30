9cb54a80e393b6d484813f0f2a46f844fe4c7809
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-06-30 12:52:41 +0100

    Removed unnecessary piping from bash script

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a DELETE request to the URL, and display the body of the response
-[ "$(curl -sI -w "%{http_code}" -X "GET" --url "$(echo "$1" | awk -F'/' '{print $1}')" | tail -n 1)" == "200" ] && curl -s -L -X "DELETE" --url "$1"
+curl -s -X "DELETE" --url "$1"
