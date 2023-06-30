ea8d74d8493175d5a015d97e9738a82ac685990d
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-06-30 12:41:59 +0100

    Removed new line from output

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a request to a URL passed as an argument, and displays the status code of the response
-curl -s -o /dev/null -w "%{http_code}\n" --url "$1"
+curl -s -o /dev/null -w "%{http_code}" --url "$1"
