7fcde32408ed7b8bf8ea9edba1d99d625e57c3f3
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-06-30 11:45:57 +0100

    Optimized code to take less memory

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,17 +8,13 @@ def find_peak(nums):
     """Finds and returns a peak in a list of unsorted integers"""
     if not nums:
         return (None)
-    peak = []
     size = len(nums)
     if size > 1 and nums[0] > nums[1]:
-        peak.append(nums[0])
+        return (nums[0])
     if size > 1 and nums[size - 1] > nums[size - 2]:
-        peak.append(nums[size - 1])
+        return (nums[size - 1])
 
     for i in range(1, size - 1):
         if nums[i] > nums[i - 1] and nums[i] > nums[i + 1]:
-            peak.append(nums[i])
-    if len(peak) > 0:
-        return (max(peak))
-    else:
-        return (nums[0])
+            return (nums[i])
+    return (nums[0])
