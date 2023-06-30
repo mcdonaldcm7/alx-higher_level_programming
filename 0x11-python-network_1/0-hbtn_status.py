c0693872c3e062b13ba2b56845aa758ebba5935b
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-06-30 16:14:02 +0100

    Fetches 'https://alx-intranet.hbtn.io/status' and prints some contents about it

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This script fetches 'https://alx-intranet.hbtn.io/status'"""
+import urllib.request
+
+
+with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+    print("Body response:")
+    print("\t- type: {}".format(type(response.status)))
+    print("\t- content: {}".format(response.read()))
+    print("\t- utf8 content: {}".format(response.msg))
