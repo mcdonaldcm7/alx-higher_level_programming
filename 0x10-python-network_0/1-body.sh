50fed4c761debae1ed209f44a7b7c3ddbffdf19b
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-06-30 12:47:44 +0100

    Optimized removed "GET"

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a GET request to the URL, and display the body of the response
-[ "$(curl -s -o /dev/null -w "%{http_code}" -X "GET" --url "$1")" == "200" ] && curl -s -L -X "GET" --url "$1"
+[ "$(curl -s -o /dev/null -w "%{http_code}" --url "$1")" == "200" ] && curl -s -L -X "GET" --url "$1"
