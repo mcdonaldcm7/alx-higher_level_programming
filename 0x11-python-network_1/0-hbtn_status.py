a54ca587a40253b9e427a6dde673801c14e67653
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-06-30 16:17:58 +0100

    Checked type of read()

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,6 +5,6 @@ import urllib.request
 
 with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
     print("Body response:")
-    print("\t- type: {}".format(type(response.status)))
+    print("\t- type: {}".format(type(response.read())))
     print("\t- content: {}".format(response.read()))
     print("\t- utf8 content: {}".format(response.msg))
