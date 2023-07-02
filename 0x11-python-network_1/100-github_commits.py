e6c0ac97a65ff3881c14bab1249f29b7a7a4d74f
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-07-02 15:21:15 +0100

    A python script that list 10 commits (from the most recent to oldes) from the repo "rails" by the user "rails"

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This script prints 10 commits (from the most recent to oldest) of the
+repository \"rails\" by the user \"rails\"
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+            sys.argv[2], sys.argv[1]
+            )
+    r = requests.get(url=url)
+    res = r.json()
+    count = 0
+    for key in res:
+        if (count > 10):
+            break
+        print("{}: {}".format(
+            key.get("sha"),
+            key.get("commit").get("author").get("name")))
+        count += 1
