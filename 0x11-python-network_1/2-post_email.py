f9fc445c9bfc2023ce9063beab04ef8726cd8c3c
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-07-02 16:32:51 +0100

    Corrected argument passes to urlopen()

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -12,6 +12,6 @@ if __name__ == "__main__":
     value = {"email": sys.argv[2]}
     data = urllib.parse.urlencode(value)
     data = data.encode("ascii")
-    req = urllib.request.Request(sys.argv[1], sys.argv[2])
+    req = urllib.request.Request(sys.argv[1], data)
     with urllib.request.urlopen(req) as response:
-        print(response.read().decode("utf-8"), end="")
+        print(response.read().decode("utf-8"))
