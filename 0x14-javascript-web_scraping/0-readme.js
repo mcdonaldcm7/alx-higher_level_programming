b6f82a39e6abbc61d5b55e09aed342a8ab879d3a
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-07-25 21:28:06 +0100

    Removed text before error message

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf8', (err, data) => {
   if (err) {
-    console.error('Error reading the file:', err);
+    console.error(err);
   } else {
     console.log(data);
   }
