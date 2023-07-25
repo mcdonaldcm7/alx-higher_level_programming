91bc480f57d3b9d2afa042a65483a9185dac6cc7
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-07-25 21:59:18 +0000

    Used error handler

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
 const request = require('request');
-request.get(process.argv[2], function (error, response, body) {
-  console.log('code: ', response.statusCode);
+request.get(process.argv[2], function (err, response, body) {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log('code: ', response.statusCode);
+  }
 });
