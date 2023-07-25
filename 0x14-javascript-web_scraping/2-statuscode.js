8753804985bbe44a035375bf6aa0affd6754dc07
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-07-25 21:57:28 +0000

    Removed error handler

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
 const request = require('request');
 request.get(process.argv[2], function (error, response, body) {
-  if (error) {
-    console.error(error);
-  } else {
-    console.log('code: ', response.statusCode);
-  }
+  console.log('code: ', response.statusCode);
 });
