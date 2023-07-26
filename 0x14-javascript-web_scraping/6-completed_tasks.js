073898cb8e1f851921e1fac2cae8d0d614701678
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-07-26 06:14:04 +0100

    Made code semistandard approved

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,22 +1,22 @@
 #!/usr/bin/node
-const request = require('request')
+const request = require('request');
 request(process.argv[2], function (error, response, body) {
   if (error) {
     console.error(error);
   } else {
     const result = JSON.parse(body);
-    let tasks_stat = {}
+    const taskStat = {};
     for (const todo of result) {
-      tasks_stat[todo.userId] = 0;
+      taskStat[todo.userId] = 0;
     }
     for (const todo of result) {
-      tasks_stat[todo.userId] += Number(todo.completed);
+      taskStat[todo.userId] += Number(todo.completed);
     }
-    for (const i in tasks_stat) {
-      if (tasks_stat[i] === 0) {
-        delete tasks_stat[i];
+    for (const i in taskStat) {
+      if (taskStat[i] === 0) {
+        delete taskStat[i];
       }
     }
-    console.log(tasks_stat);
+    console.log(taskStat);
   }
 });
