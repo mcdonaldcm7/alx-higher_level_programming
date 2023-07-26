1610a1636632dfb669ca358b0b005f557bf255f6
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-07-26 12:49:04 +0100

    Made code semi-standard approved

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -3,7 +3,7 @@
 // to fetch their name(s) :)
 const request = require('request');
 const url = (
-'https://swapi-api.alx-tools.com/api/films/' + process.argv[2] + ' /'
+  'https://swapi-api.alx-tools.com/api/films/' + process.argv[2] + ' /'
 );
 request(url, function (err, response, body) {
   if (err) {
