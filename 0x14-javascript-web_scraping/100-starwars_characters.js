cce4c4f6ae4396567854b578d3d76db8258c0ca5
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-07-26 12:43:01 +0100

    Use the command-line argument to specify the episode

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -2,7 +2,9 @@
 // Use the character list to get the URL for each person the send a GET request
 // to fetch their name(s) :)
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/films/3/';
+const url = (
+'https://swapi-api.alx-tools.com/api/films/' + process.argv[2] + ' /'
+);
 request(url, function (err, response, body) {
   if (err) {
     console.error(err);
