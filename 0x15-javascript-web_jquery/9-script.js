80bf10c4ab4dbb62ff191bcf43896abbdcb228e4
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-08-03 18:04:52 +0100

    Resolved CORS error

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
-  $.getJSON('https://fourtonfish.com/hellosalut/?lang=fr?callback=?',
-    function (data, textStatus) {
-      console.log(data);
-      //$('DIV#hello').html(data.hello);
-    });
+  $.getJSON('https://hellosalut.stefanbohacek.dev/?lang=fr', function (data) {
+    $('DIV#hello').text(data.hello);
+  });
 });
